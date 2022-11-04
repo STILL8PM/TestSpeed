18e7ed3f6a04a15403c30fb4564105399e95244d
still8pm <1040479402@qq.com>
2022-11-04 09:06:38 +0800

    1

--- src/components/SpeedChart.js
+++ src/components/SpeedChart.js
@@ -1,12 +1,39 @@
-import React, { PureComponent } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import * as eCharts from "echarts";
-import { Button } from 'antd'
-export default class SpeedChart extends PureComponent {
+import { Button } from 'antd';
+import test from './client';
+const SpeedChart = () => {
+    const [speedValue, setSpeedValue] = useState(0)
+    const eChartsRef = useRef()
+    const speedFun = () => {
+        setSpeedValue(test() * 8)
+    }
+    const getSpeedWithAjax = async (url) => {
+        try {
+            return await new Promise((resolve, reject) => {
+                let start = null;
+                let end = null;
+                start = new Date().getTime();
+                const xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        end = new Date().getTime();
+                        const size = xhr.getResponseHeader('Content-Length') / 1024;
+                        const speed = size * 1000 / (end - start);
+                        resolve(speed);
+                        console.log(speed);
+                    }
+                };
+                xhr.open('GET', url);
+                xhr.send();
+            });
+        } catch (err) {
+            throw err;
+        }
+    }
+    useEffect(() => {
 
-    eChartsRef = React.createRef();
-
-    componentDidMount() {
-        const myChart = eCharts.init(this.eChartsRef.current);
+        const myChart = eCharts.init(eChartsRef.current);
 
         let option = {
             tooltip: {
@@ -25,33 +52,40 @@ export default class SpeedChart extends PureComponent {
                     },
                     data: [
                         {
-                            value: 50,
-                            name: '网速( MB/s )'
+                            value: speedValue,
+                            name: ' Mbps '
                         }
-                    ]
+                    ],
+                    min: 0,
+                    max: 1000,
                 }
             ]
         };
 
         myChart.setOption(option);
-    }
+    }, [speedValue])
 
-    render() {
-        return <div>
-            <Button type="primary">开始测速</Button>
-            <div ref={this.eChartsRef} style={{
-                width: 600,
-                height: 400,
 
-            }}></div>
 
-            <div style={{ width: 600, }}>
-                <div style={{ display: 'flex', justifyContent: "space-around" }}>
 
-                    <span>上行网速：{}</span>
-                    <span>上行网速：{}</span>
-                </div>
+    return <div>
+        <Button type="primary" onClick={() => {
+            speedFun()
+            getSpeedWithAjax('www.baidu.com')
+        }}>开始测速</Button>
+        <div ref={eChartsRef} style={{
+            width: 600,
+            height: 400,
+
+        }}></div>
+        <div style={{ width: 600, }}>
+            <div style={{ display: 'flex', justifyContent: "space-around" }}>
+
+                <span>上行网速：{speedValue}MB/s</span>
+                <span>上行网速：{speedValue}MB/s</span>
             </div>
         </div>
-    }
+    </div>
+
 }
+export default SpeedChart 
